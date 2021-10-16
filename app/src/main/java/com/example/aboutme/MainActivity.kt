cb0da6339d28f06ad964e94678a6fb9dec96cb2e
Iliyan-Y <maximizebg@gmail.com>
2021-10-16 17:00:29 +0100

    Functionality added

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    val doneButton = findViewById<Button>(R.id.done_button)
+    doneButton.setOnClickListener {
+      swapText()
+      doneButton.isEnabled = false
     }
+  }
+
+  private fun swapText() {
+    val nickNameEdit = findViewById<TextView>(R.id.nickname_edit)
+    val nickNameDisplay = findViewById<TextView>(R.id.nick_name)
+
+    nickNameDisplay.text = nickNameEdit.text
+
+    nickNameEdit.visibility = View.GONE
+    nickNameDisplay.visibility = View.VISIBLE
+    //hide keyboard
+    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+  }
+
 }
